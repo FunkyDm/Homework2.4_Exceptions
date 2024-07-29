249ddb0a9af5bd6fe46b245733a8df46ab43da56
PoshiloySmih <mrforxbox@mail.ru>
2024-07-29 14:07:50 +0300

    Выполнены все задания домашней работы

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,31 @@
 import exceptions.WrongLoginException;
+import exceptions.WrongPasswordException;
 
 public class Main {
     public static final String ALLOWED_SYMBOLS = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890_";
 
     public static void main(String[] args) {
-        
+        try {
+            checkAuthentication("askjdjas123_", "qwerty123_", "qwerty123_");
+            System.out.println("Успешная аутентификация пользователя.");
+        } catch (WrongLoginException | WrongPasswordException exception) {
+            System.out.println(exception.getMessage());
+        }
     }
 
-    private static void checkAuthentication(String login, String password, String confirmPassword){
-        if(login.length() > 20){
-            throw new WrongLoginException("Неправильный логин");
+    private static void checkAuthentication(String login, String password, String confirmPassword) {
+        for (int i = 0; i < login.length(); i++) {
+            if (!ALLOWED_SYMBOLS.contains(Character.toString(login.charAt(i))) || login.length() > 20) {
+                throw new WrongLoginException("Неправильный логин.");
+            }
         }
-        for(int i = 0; i < login.length(); i++){
-
+        for (int i = 0; i < password.length(); i++) {
+            if (!ALLOWED_SYMBOLS.contains(Character.toString(password.charAt(i))) || password.length() > 20) {
+                throw new WrongPasswordException("Неправильный пароль.");
+            }
+        }
+        if (!(password.equals(confirmPassword))) {
+            throw new WrongPasswordException("Пароли не совпадают.");
         }
     }
 }
---
+++ src/exceptions/WrongPasswordException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class WrongPasswordException extends RuntimeException{
+    public WrongPasswordException(String message) {
+        super(message);
+    }
+}
